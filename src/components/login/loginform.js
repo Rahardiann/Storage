2fc0675a5780cd8b8d78313381d2a71985502adc
radityohk <ditoradityo6@gmail.com>
2024-01-24 13:43:44 +0700

    login

--- src/components/login/loginform.js
+++ src/components/login/loginform.js
@@ -1,35 +1,57 @@
 // LoginForm.js
-import React from 'react';
+import {useState, react} from 'react';
 import '../login/loginform.css';
 import backgroundImage from '../../assets/login.png'; 
 import logoSVG from '../../assets/logologin.svg';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const LoginForm = () => {
-  // const [username, setUsername] = useState('');
-  // const [password, setPassword] = useState('');
+  const [user, setUser] = useState({
+    username: "",
+    password: ""
+})
+  const handleChange = (e) => {
+    setUser(prev => ({ ...prev, [e.target.name]: e.target.value }))
+}
 
-  // const handleLogin = () => {
-  //   // Logika login dapat ditambahkan di sini
-  //   console.log('Username:', username);
-  //   console.log('Password:', password);
-  // };
+  const handleLogin = async (e) => {
+    e.preventDefault()
+    let data = {
+      username: user.username,
+      password: user.password
+    }
+
+    try{
+      const response = await axios.get('http://localhost:8000/user/login', data)
+  
+      if (response.data.length === 0) {
+        toast.info("Username / Password tidak ditemukan")
+      } else {
+        toast.success("Login Berhasil!")
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error("Login Error")
+    }
+  }
 
   return (
     <div className="login-form">
       <div className="form-container">
         <img src={logoSVG} alt="Logo" className="logo" />
         <h2>Login</h2>
-        <form>
+        <form onSubmit={handleLogin}>
           <div className="label-container">
             <label>
-              <input type="text" placeholder="username" />
+              <input type="text" placeholder="username" name="username" id="username" value={user.username} onChange={handleChange}/>
             </label>
             <label>
-              <input type="password" placeholder="password" />
+              <input type="password" placeholder="password" name="password" id="password" value={user.password} onChange={handleChange}/>
             </label>
           </div>
           <div className="button-container">
-            <button type="button">Continue</button>
+            <button type="submit">Continue</button>
           </div>
         </form>
       </div>
