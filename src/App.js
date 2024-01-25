be41cbd5abb766433e2bb332c62408b7d5317ac6
radityohk <ditoradityo6@gmail.com>
2024-01-25 10:02:56 +0700

    bar chart

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import {Dashboard, LoginForm} from './pages'
+import BarChartComponent from './pages/homepage/dashboard';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginForm/>}/>
-        <Route path="/home" element={<Dashboard/>}/>
+        <Route path="/home" element={<BarChartComponent/>}/>
       </Routes>
     </Router>
   );
--- src/pages/homepage/dashboard.js
+++ src/pages/homepage/dashboard.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import Sidebar from '../sidebar/sidebar';
 import "../homepage/dashboard.css"
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 
 const Dashboard = () => {
@@ -53,14 +53,116 @@ const Dashboard = () => {
   return (
     <div className="dashboard-container">
   <Sidebar />
-  <ResponsiveContainer width="100%" height="100%">
-    <BarChart width={150} height={40} data={data}>
-      <Bar dataKey="uv" fill="#8884d8" />
-    </BarChart>
-  </ResponsiveContainer>
+        <BarChart
+          width={950}
+          height={500}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="pv" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
+          <Bar dataKey="uv" fill="#82ca9d" activeBar={<Rectangle fill="gold" stroke="purple" />} />
+        </BarChart>
+
+  {/* <Sidebar/>
+//       <BarChart width={950} height={500} data={data}>
+//         <CartesianGrid strokeDasharray="3 3" />
+//         <XAxis dataKey="uv" interval={0} textAnchor="middle" />
+//         <YAxis />
+//         <Tooltip />
+//         <Legend />
+//         <Bar dataKey="pv" fill="#8884d8" name="Total Pembelian" />
+//       </BarChart> */}
 </div>
 
   );
 };
 
 export default Dashboard;
+
+// Dashboard.js
+// import React from "react";
+// import Sidebar from "../sidebar/sidebar";
+// import "../homepage/dashboard.css";
+// import {
+//   BarChart,
+//   Bar,
+//   Cell,
+//   XAxis,
+//   YAxis,
+//   CartesianGrid,
+//   Tooltip,
+//   Legend,
+//   ResponsiveContainer,
+// } from "recharts";
+
+// const Dashboard = () => {
+//   const data = [
+//     {
+//       name: "Page A",
+//       uv: 4000,
+//       pv: 2400,
+//       amt: 2400,
+//     },
+//     {
+//       name: "Page B",
+//       uv: 3000,
+//       pv: 1398,
+//       amt: 2210,
+//     },
+//     {
+//       name: "Page C",
+//       uv: 2000,
+//       pv: 9800,
+//       amt: 2290,
+//     },
+//     {
+//       name: "Page D",
+//       uv: 2780,
+//       pv: 3908,
+//       amt: 2000,
+//     },
+//     {
+//       name: "Page E",
+//       uv: 1890,
+//       pv: 4800,
+//       amt: 2181,
+//     },
+//     {
+//       name: "Page F",
+//       uv: 2390,
+//       pv: 3800,
+//       amt: 2500,
+//     },
+//     {
+//       name: "Page G",
+//       uv: 3490,
+//       pv: 4300,
+//       amt: 2100,
+//     },
+//   ];
+//   return (
+//     <div className="pt-16">
+//       <Sidebar/>
+//       <BarChart width={950} height={500} data={data}>
+//         <CartesianGrid strokeDasharray="3 3" />
+//         <XAxis dataKey="uv" interval={0} textAnchor="middle" />
+//         <YAxis />
+//         <Tooltip />
+//         <Legend />
+//         <Bar dataKey="uv" fill="#8884d8" name="Total Pembelian" />
+//       </BarChart>
+//     </div>
+//   );
+// };
+
+// export default Dashboard;
