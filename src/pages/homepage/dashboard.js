4495e83d77f25ec0b8ac12059315c4a141c50a85
Rahardiann <yt005881@gmail.com>
2024-02-02 10:20:50 +0700

    dashboard dan sidebar

--- src/pages/homepage/dashboard.js
+++ src/pages/homepage/dashboard.js
@@ -1,157 +1,101 @@
-// Dashboard.js
 import React from 'react';
 import Sidebar from '../sidebar/sidebar';
-// import "../homepage/dashboard.css"
-import { BarChart, Bar, Rectangle, CartesianGrid, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer, PieChart, Pie, Cell } from 'recharts'; // Impor Cell dari Recharts
+import {
+  BarChart,
+  Bar,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+  PieChart,
+  Pie,
+  Cell
+} from 'recharts';
 
 const Dashboard = () => {
   const data = [
-    {
-      name: 'Januaro',
-      Barang_Masuk: 4000,
-      pv: 2400,
-      Barang_keluar: 2400,
-    },
-    {
-      name: 'Februari',
-      Barang_Masuk: 3000,
-      pv: 1398,
-      Barang_keluar: 2210,
-    },
-    {
-      name: 'maret',
-      Barang_Masuk: 2000,
-      pv: 9800,
-      Barang_keluar: 2290,
-    },
-    {
-      name: 'April',
-      Barang_Masuk: 2780,
-      pv: 3908,
-      Barang_keluar: 2000,
-    },
-    {
-      name: 'Mei',
-      Barang_Masuk: 1890,
-      pv: 4800,
-      Barang_keluar: 2181,
-    },
-    {
-      name: 'Juni',
-      Barang_Masuk: 2390,
-      pv: 3800,
-      Barang_keluar: 2500,
-    },
-    {
-      name: 'Juli',
-      Barang_Masuk: 3490,
-      pv: 4300,
-      Barang_keluar: 2100,
-    },
-    {
-      name: 'agustus',
-      Barang_Masuk: 3490,
-      pv: 4300,
-      Barang_keluar: 2100,
-    },
-    {
-      name: 'september',
-      Barang_Masuk: 340,
-      pv: 4300,
-      Barang_keluar: 2100,
-    },
-    {
-      name: 'oktober',
-      Barang_Masuk: 3420,
-      pv: 4300,
-      Barang_keluar: 2100,
-    },
-    {
-      name: 'november',
-      Barang_Masuk: 1490,
-      pv: 4300,
-      Barang_keluar: 2100,
-    },
-    {
-      name: 'desember',
-      Barang_Masuk: 5490,
-      pv: 4300,
-      Barang_keluar: 2100,
-    },
-  ]
-  
-  const anj = [
-    { name: "Geeksforgeeks", students: 400 },
-    { name: "Technical scripter", students: 700 },
-    { name: "Geek-i-knack", students: 200 },
-    { name: "Geek-o-mania", students: 1000 },
-] ;
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+    { name: 'Januaro', Barang_Masuk: 4000, pv: 2400, Barang_keluar: 2400 },
+    { name: 'Februari', Barang_Masuk: 3000, pv: 1398, Barang_keluar: 2210 },
+    { name: 'maret', Barang_Masuk: 2000, pv: 9800, Barang_keluar: 2290 },
+    { name: 'April', Barang_Masuk: 2780, pv: 3908, Barang_keluar: 2000 },
+    { name: 'Mei', Barang_Masuk: 1890, pv: 4800, Barang_keluar: 2181 },
+    { name: 'Juni', Barang_Masuk: 2390, pv: 3800, Barang_keluar: 2500 },
+    { name: 'Juli', Barang_Masuk: 3490, pv: 4300, Barang_keluar: 2100 },
+    { name: 'agustus', Barang_Masuk: 3490, pv: 4300, Barang_keluar: 2100 },
+    { name: 'september', Barang_Masuk: 340, pv: 4300, Barang_keluar: 2100 },
+    { name: 'oktober', Barang_Masuk: 3420, pv: 4300, Barang_keluar: 2100 },
+    { name: 'november', Barang_Masuk: 1490, pv: 4300, Barang_keluar: 2100 },
+    { name: 'desember', Barang_Masuk: 5490, pv: 4300, Barang_keluar: 2100 },
+  ];
 
-// Fungsi untuk menampilkan label kustom di dalam pie chart
-const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
-  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-  const x = cx + radius * Math.cos(-midAngle * Math.PI / 180);
-  const y = cy + radius * Math.sin(-midAngle * Math.PI / 180);
+  const anj = [
+    { name: 'Geeksforgeeks', students: 400 },
+    { name: 'Technical scripter', students: 700 },
+    { name: 'Geek-i-knack', students: 200 },
+    { name: 'Geek-o-mania', students: 1000 },
+  ];
+  
+  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
+  const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * Math.PI / 180);
+    const y = cy + radius * Math.sin(-midAngle * Math.PI / 180);
+
+    return (
+      <text x={x} y={y} fill="white" textAnchor="middle" dominantBaseline="central">
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
 
   return (
-    <text x={x} y={y} fill="white" textAnchor="middle" dominantBaseline="central">
-      {`${(percent * 100).toFixed(0)}%`}
-    </text>
-  );
-};
-
-return (
-  <div className="flex h-screen">
-    <Sidebar />
-    <div className="flex-1 p-4 md:p-8 lg:p-12 xl:p-16">
-      <div className="bg-blue-200 p-4 md:p-8 lg:p-12 xl:p-16 rounded shadow-lg">
-        <ResponsiveContainer width="100%" height={300}>
-          <BarChart
-            data={data}
-            margin={{
-              top: 10,
-              right: 20,
-              left: 20,
-              bottom: 5,
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="Barang_Masuk" fill="#8884d8" />
-          <Bar dataKey="Barang_keluar" fill="#82ca9d"  />
-          </BarChart>
-        </ResponsiveContainer>
-      </div>
-      <div className="flex-1 p-4 md:p-8 lg:p-12 xl:p-16">
-        <ResponsiveContainer width="100%" height={300}>
-          <PieChart width={800} height={800}>
-            <Pie
-              data={anj}
-              cx="60%"
-              cy="45%"
-              labelLine={false}
-              label={renderCustomizedLabel}
-              outerRadius={130}
-              fill="#8884d8"
-              dataKey="students"
-            >
-              {data.map((entry, index) => (
-                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-              ))}
-            </Pie>
-          </PieChart>
-        </ResponsiveContainer>
+    <div className="flex h-screen">
+      <Sidebar />
+      <div className="flex-1 overflow-y-auto">
+        <div className="p-4 md:p-8 lg:p-12 xl:p-16">
+          <div className="bg-blue-200 p-4 md:p-8 lg:p-12 xl:p-16 rounded shadow-lg">
+            <ResponsiveContainer width="100%" height={300}>
+              <BarChart
+                data={data}
+                margin={{ top: 10, right: 20, left: 20, bottom: 5 }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="Barang_Masuk" fill="#8884d8" />
+                <Bar dataKey="Barang_keluar" fill="#82ca9d" />
+              </BarChart>
+            </ResponsiveContainer>
+          </div>
+        </div>
+        <div className="p-4 md:p-8 lg:p-12 xl:p-16">
+          <ResponsiveContainer width="100%" height={300}>
+            <PieChart width={800} height={800}>
+              <Pie
+                data={anj}
+                cx="50%"
+                cy="50%"
+                labelLine={false}
+                label={renderCustomizedLabel}
+                outerRadius={130}
+                fill="#8884d8"
+                dataKey="students"
+              >
+                {anj.map((entry, index) => (
+                  <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                ))}
+              </Pie>
+            </PieChart>
+          </ResponsiveContainer>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
 };
-
-
-
 
 export default Dashboard;
--- src/pages/sidebar/sidebar.js
+++ src/pages/sidebar/sidebar.js
@@ -1,55 +1,64 @@
-// sidebar.js
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { FaHome, FaBox, FaCubes, FaHistory, FaSignOutAlt } from 'react-icons/fa';
+import { FaHome, FaBox, FaCubes, FaHistory, FaSignOutAlt, FaBars } from 'react-icons/fa';
 import Logosidebar from '../../assets/logosidebar.svg';
 
 const Sidebar = () => {
-  const [minimized, setMinimized] = useState(false);
+  const [minimized, setMinimized] = useState(window.innerWidth <= 768);
 
-  const toggleSidebar = () => {
-    setMinimized(!minimized);
-  };
+  useEffect(() => {
+    const handleResize = () => {
+      setMinimized(window.innerWidth <= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <div className={`flex flex-col h-screen ${minimized ? 'w-16' : 'w-64'} bg-main transition-all ease-in-out duration-300`}>
       <div className="p-4">
-        <img src={Logosidebar} alt="Logo" className={`size-32  w-full ${minimized ? 'mx-auto' : 'mt-8'}`} />
+        <img src={Logosidebar} alt="Logo" className={`size-32 w-full ${minimized ? 'mx-auto' : 'mt-8'}`} />
       </div>
       <ul className="flex-grow">
         <li className="my-4">
-          <Link to="/dashboard" className={`flex items-center text-white text-base sidebar-item ${minimized ? 'pl-2' : 'pl-4'}  hover:text-gray-400 `}>
+          <Link to="/dashboard" className={`flex items-center text-white text-base sidebar-item ${minimized ? 'pl-2' : 'pl-4'} hover:text-gray-400`} onClick={() => setMinimized(true)}>
             <FaHome className={`mr-2 ${minimized ? 'ml-3' : 'mr-4'}`} /> {!minimized && <span>Dashboard</span>}
           </Link>
         </li>
         <li className="my-10">
-          <Link to="/barangjadi" className={`flex items-center text-white text-base sidebar-item ${minimized ? 'pl-2' : 'pl-4'} hover:text-gray-400`}>
+          <Link to="/barangjadi" className={`flex items-center text-white text-base sidebar-item ${minimized ? 'pl-2' : 'pl-4'} hover:text-gray-400`} onClick={() => setMinimized(true)}>
             <FaBox className={`mr-2 ${minimized ? 'ml-3' : 'mr-4'}`} /> {!minimized && <span>Stock Barang Jadi</span>}
           </Link>
         </li>
         <li className="my-10">
-          <Link to="/stock-barang-mentah" className={`flex items-center text-white text-base sidebar-item ${minimized ? 'pl-2' : 'pl-4'}  hover:text-gray-400`}>
+          <Link to="/stock-barang-mentah" className={`flex items-center text-white text-base sidebar-item ${minimized ? 'pl-2' : 'pl-4'} hover:text-gray-400`} onClick={() => setMinimized(true)}>
             <FaCubes className={`mr-2 ${minimized ? 'ml-3' : 'mr-4'}`} /> {!minimized && <span>Stock Barang Mentah</span>}
           </Link>
         </li>
         <li className="my-10">
-          <Link to="/riwayat" className={`flex items-center text-white text-base sidebar-item ${minimized ? 'pl-2' : 'pl-4'} hover:text-gray-400`}>
+          <Link to="/riwayat" className={`flex items-center text-white text-base sidebar-item ${minimized ? 'pl-2' : 'pl-4'} hover:text-gray-400`} onClick={() => setMinimized(true)}>
             <FaHistory className={`mr-2 ${minimized ? 'ml-3' : 'mr-4'}`} /> {!minimized && <span>Riwayat</span>}
           </Link>
         </li>
         <li className="my-10">
-          <Link to="/logout" className={`flex items-center text-white text-base sidebar-item ${minimized ? 'pl-2' : 'pl-4'}  hover:text-gray-400`}>
+          <Link to="/logout" className={`flex items-center text-white text-base sidebar-item ${minimized ? 'pl-2' : 'pl-4'} hover:text-gray-400`} onClick={() => setMinimized(true)}>
             <FaSignOutAlt className={`mr-2 ${minimized ? 'ml-3' : 'mr-4'}`} /> {!minimized && <span>Logout</span>}
           </Link>
         </li>
       </ul>
-      <div className="p-4">
-        <button className="bg-blue-200 text-main btn btn-blue  py-2 rounded" onClick={toggleSidebar}>
-          {minimized ? '>' : '<'}
-        </button>
-      </div>
+      {window.innerWidth <= 768 && !minimized && (
+        <div className="p-4">
+          <button className="bg-blue-200 text-main btn btn-blue py-2 rounded" onClick={() => setMinimized(true)}>
+            <FaBars />
+          </button>
+        </div>
+      )}
     </div>
   );
 };
+
 export default Sidebar;
