3472c3310a654a3921d88b59df1e48158fdfd51a
Rahardiann <yt005881@gmail.com>
2024-05-20 09:59:37 +0700

    update

--- src/pages/master/dentist.js
+++ src/pages/master/dentist.js
@@ -16,9 +16,14 @@ function Masterbarangjadi() {
   const [listBarang, setListBarang] = useState([]);
   const [kodebarang, setKodebarang] = useState("");
   const [spesialist, setSpesialist] = useState("");
+  const [showFormedit, setShowFormedit] = useState(false);
   const [id, setIDDentist] = useState("");
   const [editingIndex, setEditingIndex] = useState(null);
 
+  const handleCloseFormEdit = () => {
+    setShowFormedit(false);
+  };
+  
   const handleAddBarang = () => {
     const newBarang = {
       kategori: kategoriBarang,
@@ -71,7 +76,7 @@ function Masterbarangjadi() {
     setFotoBarang(barang.email);
     setSpesialist(barang.spesialist);
     setEditingIndex(index);
-    setShowForm(true);
+    setShowFormedit(true);
   };
 
   const handleDeleteBarang = (index) => {
@@ -142,12 +147,12 @@ function Masterbarangjadi() {
             />
           </div>
 
-          {showForm && (
+          {showFormedit && (
             <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-8 rounded-lg max-w-3xl w-full">
               <div className="bg-main text-white font-bold rounded-t-lg px-4 py-3 relative">
-                Master Barang Jadi
+                Edit Dentist
                 <button
-                  onClick={() => setShowForm(false)}
+                  onClick={handleCloseFormEdit}
                   className="absolute top-0 right-0 m-2 text-gray-300 font-bold"
                 >
                   <svg
@@ -197,6 +202,61 @@ function Masterbarangjadi() {
             </div>
           )}
 
+          {showForm && (
+            <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-8 rounded-lg max-w-3xl w-full">
+              <div className="bg-main text-white font-bold rounded-t-lg px-4 py-3 relative">
+                Add Dentist
+                <button
+                  onClick={() => setShowForm(false)}
+                  className="absolute top-0 right-0 m-2 text-gray-300 font-bold"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M14.293 5.293a1 1 0 00-1.414 1.414L10 10.414l-2.879-2.88a1 1 0 10-1.414 1.415L8.586 12 5.707 14.879a1 1 0 101.414 1.414L10 13.415l2.879 2.88a1 1 0 001.414-1.415L11.414 12l2.879-2.88a1 1 0 000-1.414z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <div className="bg-gray-100 shadow-lg py-4 rounded-lg p-4">
+                <input
+                  type=""
+                  value={fotoBarang}
+                  onChange={(e) => setNamaDentist(e.target.value)}
+                  placeholder="Nama Dentist"
+                  className="border border-gray-400 p-2 rounded mb-2 w-full mr-2"
+                />
+                <input
+                  type=""
+                  value={jumlahBarang}
+                  onChange={(e) => setSpesialist(e.target.value)}
+                  placeholder="No Hp"
+                  className="border border-gray-400 p-2 rounded mb-2 w-full mr-2"
+                />
+                <input
+                  type=""
+                  value={namadentist}
+                  onChange={(e) => setSpesialist(e.target.value)}
+                  placeholder="Email"
+                  className="border border-gray-400 p-2 rounded mb-2 w-full mr-2"
+                />
+
+                <button
+                  onClick={handleAddBarang}
+                  className="bg-main text-white font-bold rounded py-2 px-4 mt-4 w-full"
+                >
+                  {editingIndex !== null ? "Update Barang" : "Tambah Barang"}
+                </button>
+              </div>
+            </div>
+          )}
+
           <div className="overflow-x-auto">
             <table className="table-auto border-gray-500 w-full">
               <thead className="bg-second text-gray-500">
@@ -212,10 +272,18 @@ function Masterbarangjadi() {
               <tbody>
                 {stok.map((item, index) => (
                   <tr key={index} className="bg-second">
-                    <td className="text-center border-gray-500 px-2 py-2">{item.id}</td>
-                    <td className="text-center border-gray-500 px-4 py-2">{item.nama}</td>
-                    <td className="text-center border-gray-500 px-4 py-2">{item.no_hp}</td>
-                    <td className="text-center border-gray-500 px-4 py-2">{item.email}</td>
+                    <td className="text-center border-gray-500 px-2 py-2">
+                      {item.id}
+                    </td>
+                    <td className="text-center border-gray-500 px-4 py-2">
+                      {item.nama}
+                    </td>
+                    <td className="text-center border-gray-500 px-4 py-2">
+                      {item.no_hp}
+                    </td>
+                    <td className="text-center border-gray-500 px-4 py-2">
+                      {item.email}
+                    </td>
                     <td className="border-gray-500 text-center py-2">
                       <button
                         onClick={() => handleEditBarang(index)}
