f5aad406ac02882aa071cde48f189fcfc2beea9a
Rahardiann <yt005881@gmail.com>
2024-06-13 16:42:41 +0700

    login

--- src/pages/login/loginform.jsx
+++ src/pages/login/loginform.jsx
@@ -1,21 +1,26 @@
 import { useState } from "react";
-import backgroundImage from "../../assets/login.png";
-import logoSVG from "../../assets/logologin.svg";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
 import axios from "../../config/axiosConfig";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import loginImage from "../../assets/logo.png";
+import "react-toastify/dist/ReactToastify.css";
 
 const LoginForm = () => {
   const navigate = useNavigate();
-
   const [user, setUser] = useState({
     email: "",
     password: "",
   });
 
+  const [showPassword, setShowPassword] = useState(false);
+
   const handleChange = (e) => {
     setUser((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleTogglePassword = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
   };
 
   const handleLogin = async (e) => {
@@ -23,68 +28,78 @@ const LoginForm = () => {
 
     try {
       const response = await axios.post("admin/login", {
-        // Adjust the endpoint as per your backend API
         email: user.email,
         password: user.password,
       });
 
-      // Assuming your backend sends back a token upon successful login
-      const token = response.data.token;
+      const token = response.data.token; // Assuming your backend sends back a token upon successful login
 
-      // You can save the token to local storage or session storage for later use
-      localStorage.setItem("token", token);
+      localStorage.setItem("token", token); // Save the token to local storage
 
-      // Redirect to the dashboard or any other page upon successful login
-      navigate("/dashboard");
+      navigate("/dashboard"); // Redirect to the dashboard or any other page upon successful login
     } catch (error) {
-      // Handle error
       console.error("Login failed:", error.message);
       toast.error("Login failed. Please check your credentials.");
     }
   };
 
   return (
-    <div className="flex h-screen">
-      <div className="flex-1 flex items-center justify-center">
-        <div className="w-80 p-20 flex flex-col items-center justify-center bg-white rounded-lg ">
-          <img src={logoSVG} alt="Logo" className="logo" />
-          <h2 className="text-2xl font-bold mb-8 text-center">Login</h2>
-          <form className="w-80 max-w-sm" onSubmit={handleLogin}>
-            <div className="w-full justify-center mb-15">
+    <div className="bg-abu min-h-screen flex items-center justify-center">
+      <div className="relative w-96 md:w-1/4">
+        <img
+          src={loginImage}
+          alt="Login"
+          className="absolute top-[-75px] left-1/2 transform -translate-x-1/2  h-40"
+        />
+        <div className="bg-custom-grey rounded-lg shadow-2xl p-8 mt-16">
+          <form onSubmit={handleLogin}>
+            {" "}
+            {/* Wrap content inside a form and handle submission */}
+            {/* Email Input */}
+            <div className="relative mb-4">
+              <h2 className="text-left text-md text-black">Email</h2>
               <input
                 type="text"
-                placeholder="email"
-                name="email"
                 id="email"
+                name="email"
+                placeholder="Enter your email"
                 value={user.email}
                 onChange={handleChange}
-                className="w-80 px-4 my-4 py-3 rounded-3xl border-2 border-gray-300 bg-gray-300"
+                required
+                className="w-full p-2 pl-2 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-400"
               />
             </div>
-            <div className="w-full mb-15">
-              <input
-                type="password"
-                placeholder="password"
-                name="password"
-                id="password"
-                value={user.password}
-                onChange={handleChange}
-                className="w-80 px-4 py-3 mb-8 rounded-3xl border-2  border-black-300 bg-gray-300"
-              />
+            {/* Password Input */}
+            <div className="relative mb-6">
+              <h2 className="text-left text-md text-black">Password</h2>
+              <div className="flex items-center border border-gray-300 rounded-lg bg-white">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  id="password"
+                  name="password"
+                  value={user.password}
+                  placeholder="Enter your password"
+                  onChange={handleChange}
+                  required
+                  className="w-full p-2 focus:outline-none"
+                />
+                <button
+                  type="button"
+                  onClick={handleTogglePassword}
+                  className="pr-2"
+                >
+                  {showPassword ? <FaEyeSlash /> : <FaEye />}
+                </button>
+              </div>
             </div>
-            <div className="flex justify-center">
-              <button
-                type="submit"
-                className="w-64 bg-main hover:bg-blue-300 text-white font-bold py-2 px-4 rounded-3xl transition duration-300"
-              >
-                Continue
-              </button>
-            </div>
+            <button
+              type="submit"
+              className="w-full py-3 text-white font-bold bg-main rounded-full hover:bg-second focus:outline-none focus:shadow-outline-gray"
+            >
+              Masuk
+            </button>
           </form>
         </div>
-      </div>
-      <div className="flex-1 bg-blue-300 flex items-center justify-center hidden sm:flex">
-        <img src={backgroundImage} alt="Background" className="max-w-full" />
       </div>
       <ToastContainer />
     </div>
